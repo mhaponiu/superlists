ae1263470ed8b0b96655b59bc2f1c687ad4c68bb
mhaponiu <m.haponiuk@stud.elka.pw.edu.pl>
2015-10-28 15:51:33 +0100

    unittest dodany

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,18 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)#czeka 3s zanim zacznie testy
+	
+    def tearDown(self):
+        self.browser.quit()
 
-assert 'Django' in browser.title
-assert False
- 
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Django', self.browser.title)
+        self.fail('Zakonczenie testu!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
