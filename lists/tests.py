959b35f94f2957cfe161ebc9f8b03ffaf274157e
mhaponiu <m.haponiuk@stud.elka.pw.edu.pl>
2015-10-29 23:55:57 +0100

    nowy branch i pierwsze minimalne testy

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,24 @@
+from django.http.request import HttpRequest
 from django.test import TestCase
+from lists.views import home_page
+from django.core.urlresolvers import resolve
+
 
 # Create your tests here.
 class SmokeTest(TestCase):
 
     def test_bad_maths(self):
-        self.assertEqual(1+1,3)
+        self.assertEqual(1+1,2)
 
+class HomePageTest(TestCase):
+
+    def test_root_url_resolvers_to_home_page(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_gome_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>Lista rzeczy do zrobienia</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,7 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.http import JsonResponse
 
+def home_page(request):
+    return HttpResponse('<html><title>Lista rzeczy do zrobienia</title></html>')
 # Create your views here.
