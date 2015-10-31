aa6b9e5623c9082b232d87ba8ca5dc4805e4ceb1
mhaponiu <m.haponiuk@stud.elka.pw.edu.pl>
2015-10-31 21:29:07 +0100

    model dla obiektów Item oraz powiązane z nim migracje

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@ from django.template.loader import render_to_string
 from django.test import TestCase
 from lists.views import home_page
 from django.core.urlresolvers import resolve
+from lists.models import Item
 
 
 # Create your tests here.
@@ -32,3 +33,22 @@ class HomePageTest(TestCase):
         response = home_page(request)
 
         self.assertIn('Nowy element listy', response.content.decode())
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retriving_items(self):
+        first_item = Item()
+        first_item.text = 'Absolutnie pierwszy element listy'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Drugi element'
+        second_item.save()
+
+        saved_item = Item.objects.all()
+        self.assertEqual(saved_item.count(), 2)
+
+        first_saved_item = saved_item[0]
+        second_saved_item = saved_item[1]
+        self.assertEqual(first_saved_item.text, 'Absolutnie pierwszy element listy')
+        self.assertEqual(second_saved_item.text, 'Drugi element')
